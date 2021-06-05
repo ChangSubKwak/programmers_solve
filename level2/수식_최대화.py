d34b759261301ef7fa4a6e9164b7a84685baf76b
ChangSubKwak <paradise1732@gmail.com>
2021-06-05 22:30:02 +0900

    Refactor 수식_최대화.py(eval을 이용하여 반복 부분 제거)

--- level2/수식_최대화.py
+++ level2/수식_최대화.py
@@ -2,7 +2,7 @@ from itertools import permutations
 import re
 
 def solution(expression):
-    permute = permutations(['*','-','+'],3)
+    permute = permutations(['*','-','+'], 3)
     opLists = list(permute)
     
     max = 0
@@ -12,26 +12,12 @@ def solution(expression):
         j = 0
         exList = re.split('(?=\*|\+|\-)|(?<=\*|\+|\-)', expression);
         while(True):
-            element = exList[i]
-            if element == '*' and opList[j] == '*':
-                resVal = str(int(exList[i-1]) * int(exList[i+1]));
+            if exList[i] in ['*','-','+'] and exList[i] == opList[j]:
+                resVal = eval(exList[i-1] + exList[i] + exList[i+1]);
                 for _ in range(3): del exList[i-1]
-                exList.insert(i-1, resVal)
+                exList.insert(i-1, str(resVal))
                 i-=1
                 continue
-            elif element == '-' and opList[j] == '-':
-                resVal = str(int(exList[i-1]) - int(exList[i+1]))
-                for _ in range(3): del exList[i-1]
-                exList.insert(i-1, resVal)
-                i-=1
-                continue;
-            elif element == '+' and opList[j] == '+':
-                resVal = str(int(exList[i-1]) + int(exList[i+1]))
-                for _ in range(3): del exList[i-1]
-                exList.insert(i-1, resVal)
-                i-=1
-                continue;        
-
             i+=1
             if i >= len(exList):
                 i = 0
@@ -39,7 +25,9 @@ def solution(expression):
                 
             if j >= 3:
                 break
+        # end of while                
         if max < abs(int(exList[0])):
             max = abs(int(exList[0]))
         
+    # print(exList)
     return max
